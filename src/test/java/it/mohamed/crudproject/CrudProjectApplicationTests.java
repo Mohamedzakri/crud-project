7fddda9a7f304303d03654622d3ef14d6fd7c15e
Mohamed <mohamed.zakri@protonmail.com>
2023-08-10 20:16:00 +0200

    Initial commit

---
+++ src/test/java/it/mohamed/crudproject/CrudProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package it.mohamed.crudproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
